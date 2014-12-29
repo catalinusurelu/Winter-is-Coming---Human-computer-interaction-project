651abc90c60cf853c3bb944fb811aa367be71f71
Catalin Constantin Usurelu <catalin.usurelu5@gmail.com>
2014-12-29 23:18:18 +0200

    Fix

--- RestfulServices/app/models.py
+++ RestfulServices/app/models.py
@@ -2,15 +2,6 @@ from hashlib import md5
 from app import db
 
 
-class UserEventsLink(db.Model):
-    __tablename__ = "user_events_link"
-    user_id = db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    event_id = db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True)
-    upvoted = event_id = db.Column('upvoted', db.Boolean, default = False)
-    user = db.relationship(User, backref=backref("user_assoc"))
-    event = db.relationship(Event, backref=backref("event_assoc"))
-
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(127), index=True)
@@ -23,7 +14,7 @@ class User(db.Model):
     tokens = db.relationship('Token', backref='owner', lazy='dynamic')
 
     attending = db.relationship('Event',
-                               secondary=user_events_link,
+                               secondary='user_events_link',
                                backref=db.backref('participants', lazy='dynamic'),
                                lazy='dynamic')
 
@@ -38,13 +29,6 @@ class User(db.Model):
         dictionary['password'] = self.password
 
         return dictionary
-
-
-class Token(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    token = db.Column(db.String(32))
-
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -73,3 +57,20 @@ class Event(db.Model):
         dictionary['event_status'] = self.event_status
 
         return dictionary
+
+
+class UserEventsLink(db.Model):
+    __tablename__ = "user_events_link"
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    event_id = db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True)
+    upvoted = event_id = db.Column('upvoted', db.Boolean, default = False)
+    user = db.relationship(User, backref=backref("user_assoc"))
+    event = db.relationship(Event, backref=backref("event_assoc"))
+
+
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    token = db.Column(db.String(32))
+
+
